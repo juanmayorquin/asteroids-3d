03e1b3e553010ee2ca22b4aff83cc9885fe7c37b
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2023-04-28 12:52:48 -0500

    Camara corregida, menu corregido

--- Asteroids/Assets/Scripts/Camera.cs
+++ Asteroids/Assets/Scripts/Camera.cs
@@ -17,10 +17,10 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 toPos = player.transform.position + (player.transform.rotation * distance);
+        Vector3 toPos = player.transform.position + (player.transform.rotation * distance * 1.25f);
         transform.position = Vector3.SmoothDamp(transform.position, toPos, ref velocity, 0.1f);
 
-        Quaternion toRot = Quaternion.LookRotation(player.transform.position - transform.position, player.transform.up);
+        Quaternion toRot = Quaternion.LookRotation((player.transform.position - transform.position), player.transform.up);
         transform.rotation = Quaternion.Slerp(transform.rotation, toRot, 10f * Time.deltaTime);
     }
 }
